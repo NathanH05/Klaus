b43cca9bebbfafc5f7eb9cb37ddf61966e604fe3
NathanH05 <nathanhampshire@Nathans-MBP.station>
2016-06-30 00:00:01 +1200

    Adding new Klaus site

--- script.js
+++ script.js
@@ -2,27 +2,55 @@
 
 
 //var sel = document.getElementById('typeSelect');
+var loop;
+var saleLabel =document.getElementsByTagName("sally");
 
-
+    var sels = document.getElementsByTagName("typeSelect");
+    for(var i = 0, maxI = sels.length; i < maxI; ++i) {
+      var sel = sels[i];
+      if(sel == slctFild)
+        continue;  // exclude the current select element
+      sel.selectedIndex = 0;
+    };
+var loop =0;
 function preparePage() {
 	// used throughout the examples below
 
 // used throughout the examples below
-var sel = document.getElementById('typeSelect');
-var saleLabel =document.getElementById("sally");
+
                                       
+	loop++;
+loop.onchange = function(){
 
+setTimeout(preparePage, 1000);
+}
+if (document.getElementById("typeSelect").selectedIndex == "0"){
+sally.setAttribute("display","none");
+change = "select Index must be 0";
+                                    
+	loop++;
+loop.onchange = function(){
 
-if (sel.selectedIndex == "0"){
-saleLabel.style.display="none";
-  
+setTimeout(preparePage, 1000);
+}
 }
 else{
-  saleLabel.style.display="inline-block";
-
+  sally.setAttribute=("display","inline-block");
+ change ="select Index must be 1=<";
+                                  
+	loop++;
+loop.onchange = function(){
+console.log(loop);
+setTimeout(preparePage, 1000);
 }  
-//};
-//console.log(sel.selectedIndex);
-	
-}
-document.getElementById('typeSelect').onchange = "preparePage(this);"
+
+
+};
+
+
+};window.onload = 
+function(){
+
+setTimeout(preparePage, 1000);
+
+};
